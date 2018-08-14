32f57d477fd2f176cbe1db6a4347308ba04bcc9b
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-08-14 17:22:40 -0600

    added trainer

---
+++ trainer/db/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Exercise = require("../models/exercise.js");
+const ExerciseData = require("../models/exerciseData.js");
+
+// create our db and connect
+mongoose.connect('mongodb://localhost/project2');
+
+
+mongoose.connection.on('connected', () => {
+  console.log('Beyonce is here yall!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err, 'Beyonce is not ready to perform');
+});
+
+// Exercise.collection.insertMany(ExerciseData, (err, data) => {
+// 	console.log("added exercises");
+// 	mongoose.connection.close();
+// });
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Beyonce out!');
+});
